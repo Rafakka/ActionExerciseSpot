fc6a08c5c84be57b5b735d4f0d5057c7eb59d50a
Rafakka <rafaelkaher@gmail.com>
2024-11-26 11:40:22 -0300

    modificações sobre cliente

--- spotify_automation.py
+++ spotify_automation.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id="YOUR_CLIENT_ID",
     client_secret="YOUR_CLIENT_SECRET",
-    redirect_uri="http://localhost:8080",  # Set this in Spotify Dev settings
+    redirect_uri="http://localhost:8888/callback",  # Set this in Spotify Dev settings
     scope="playlist-modify-public"
 ))
 
